b187efd69c38be097302ea2d931fd9d04c6841bf
admin <xelnaga.zealot@gmail.com>
2019-09-27 11:16:16 +0930

    Updated latest snapcast source with persistent groups from frafall's repo

--- server/config.h
+++ server/config.h
@@ -28,6 +28,7 @@
 #include "common/utils/string_utils.h"
 #include "common/utils.h"
 
+#define DEFAULT_GROUP	"default"
 
 namespace strutils = utils::string;
 using json = nlohmann::json;
@@ -260,11 +261,15 @@ struct ClientInfo
 
 struct Group
 {
-	Group(const ClientInfoPtr client = nullptr) : muted(false)
+	Group(const json& j) : muted(false)
 	{
-		if (client)
-			id = client->id;
+		fromJson(j);
+	}
+
+	Group(const std::string& groupName) : muted(false)
+	{
 		id = generateUUID();
+		name = groupName;
 	}
 
 	void fromJson(const json& j)
@@ -382,10 +387,12 @@ public:
 
 //	GroupPtr removeFromGroup(const std::string& groupId, const std::string& clientId);
 //	GroupPtr setGroupForClient(const std::string& groupId, const std::string& clientId);
-
+	GroupPtr getDefaultGroup();
+	GroupPtr addGroup(const std::string& name);
 	GroupPtr getGroupFromClient(const std::string& clientId);
 	GroupPtr getGroupFromClient(ClientInfoPtr client);
 	GroupPtr getGroup(const std::string& groupId) const;
+	GroupPtr getGroupFromName(const std::string& name) const;
 
 	json getGroups() const;
 	json getServerStatus(const json& streams) const;
